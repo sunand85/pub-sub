b58d80a5e45d516c3e9271f0a22034a33cbe7f02
sunand85 <sunandonline@gmail.com>
2020-07-22 14:54:54 +0530

    Adding Reactor implementation for Pub Sub. Its more of a share feature just to simulate pub sub.

---
+++ reactor/src/main/java/com/newminds/pubsub/reactor/ProducerConsumer.java
@@ -0,0 +1,46 @@
+package com.newminds.pubsub.reactor;
+
+import reactor.core.publisher.Flux;
+
+import java.util.stream.Stream;
+
+/**
+ * Created by Sunand on 22/07/2020.
+ **/
+public class ProducerConsumer {
+  public static void main(String[] args) {
+
+    final Flux<Integer> flux = producer();
+
+    //To simulate app running in its own Thread Context
+    Runnable consumerOne = () -> consumerOne(flux);
+    new Thread(consumerOne).start();
+
+    Runnable consumerTwo = () -> consumerTwo(flux);
+    new Thread(consumerTwo).start();
+  }
+
+  private static Flux<Integer> producer() {
+    return Flux
+            .fromStream(
+                    Stream
+                            .iterate(0, i -> i + 1) //Infinite Stream
+                            .map(integer -> {
+                              try {
+                                Thread.sleep(2_000); //Adding Delay
+                              } catch (InterruptedException e) {
+                                e.printStackTrace();
+                              }
+                              return integer;
+                            })
+            ).share();
+  }
+
+  private static void consumerOne(Flux<Integer> flux) {
+    flux.subscribe(integer -> System.out.println("Received By One : " + integer));
+  }
+
+  private static void consumerTwo(Flux<Integer> flux) {
+    flux.subscribe(integer -> System.out.println("Received By Two : " + integer));
+  }
+}
